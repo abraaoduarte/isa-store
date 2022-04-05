b761f903b196d72a76a9f21321e442b209c7664a
Abraão Duarte <abraaoduarte@Abraaos-MacBook-Pro.local>
2022-04-05 18:02:19 -0300

    feat(backend): configure husky and commitlint in the project

---
+++ .lintstagedrc.js
@@ -0,0 +1,15 @@
+// https://github.com/okonet/lint-staged#how-can-i-ignore-files-from-eslintignore-
+const { CLIEngine } = require('eslint');
+
+const cli = new CLIEngine({});
+
+module.exports = {
+  './src/**/*.{js,ts}': (files) => {
+    const match = files.filter((file) => !cli.isPathIgnored(file)).join(' ');
+    return [
+      `eslint --max-warnings=0 ${match}`,
+      `prettier --write ${match}`,
+    ];
+  },
+  './**/*.{json,md,yml}': ['prettier --write'],
+};
