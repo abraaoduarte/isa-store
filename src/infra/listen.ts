13b2e1df882458ae5d487896e54cd25071c980ca
Abraão Duarte <abraaoduarte@Abraaos-MacBook-Pro.local>
2022-04-06 21:02:50 -0300

    chore(auth): add lib koa logger

--- src/infra/listen.ts
+++ src/infra/listen.ts
@@ -1,7 +1,7 @@
 import * as http from 'http';
 import type Koa from 'koa';
 
-function listen(handler: Koa, { port = 3000 } = {}): Promise<void> {
+function listen(handler: Koa, { port = 5999 } = {}): Promise<void> {
   return new Promise((resolve, reject) => {
     http
       .createServer(handler.callback())
--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,8 +1,10 @@
 import Koa from 'koa';
 import routes from 'app/api';
+import logger from 'koa-logger';
 
 const server = new Koa();
 
+server.use(logger());
 server.use(routes.routes()).use(routes.allowedMethods());
 
 export default server;
