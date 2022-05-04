d1d19fcf647296f87e4b99bc9d9ebdf73767992b
Abraão Duarte <abraaoduarte@Abraaos-MacBook-Pro.local>
2022-05-03 21:31:49 -0300

    chore(auth): parse body to json

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -2,11 +2,14 @@ import Koa from 'koa';
 import routes from 'app/api';
 import logger from 'koa-logger';
 import helmet from 'koa-helmet';
+import koaBody from 'koa-body';
 
 const server = new Koa();
 
 server.use(helmet());
 server.use(logger());
-server.use(routes.routes()).use(routes.allowedMethods());
+server.use(koaBody())
+  .use(routes.routes())
+  .use(routes.allowedMethods());
 
 export default server;
