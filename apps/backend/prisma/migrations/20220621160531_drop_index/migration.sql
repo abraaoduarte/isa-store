145a7ac8ba48784864cbc75f7b4964fa4002d157
Abraão Duarte <abraaoduarte@Abraaos-MacBook-Pro.local>
2022-06-21 13:11:34 -0300

    chore(backend): remove unique indexes

---
+++ apps/backend/prisma/migrations/20220621160531_drop_index/migration.sql
@@ -0,0 +1,20 @@
+-- DropIndex
+DROP INDEX "product_variations_color_id_key";
+
+-- DropIndex
+DROP INDEX "product_variations_product_id_key";
+
+-- DropIndex
+DROP INDEX "product_variations_size_id_key";
+
+-- DropIndex
+DROP INDEX "product_variations_user_id_key";
+
+-- DropIndex
+DROP INDEX "products_brand_id_key";
+
+-- DropIndex
+DROP INDEX "products_product_category_id_key";
+
+-- DropIndex
+DROP INDEX "products_user_id_key";
