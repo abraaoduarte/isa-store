2302fbb41cc2b548d615878f146532bb4fab197a
Abraão Duarte <abraaoduarte@Abraaos-MacBook-Pro.local>
2022-07-13 13:11:52 -0300

    chore(backend): remove console

--- apps/backend/src/domains/product-variation/product-variation-repository.ts
+++ apps/backend/src/domains/product-variation/product-variation-repository.ts
@@ -4,8 +4,6 @@ import { ProductVariation } from '@prisma/client';
 import prisma from 'prisma/prisma';
 
 export const destroy = async (uuid: string): Promise<ProductVariation> => {
-  console.log('uuid', uuid);
-  console.log('kkkkkkkkkkkkkkkkkkk', uuid);
   const productVariation = await prisma.productVariation.findUnique({
     where: {
       id: uuid
