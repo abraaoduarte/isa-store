9a4437eccc5270894dfe58adfab0e2f3ee42917f
Abraão Duarte <abraaoduarte@Abraaos-MacBook-Pro.local>
2022-07-13 13:10:54 -0300

    chore(backend): refactor product variation endpoint and remove code not used

--- apps/backend/src/app/api/product-variation/product-variation-router.ts
+++ apps/backend/src/app/api/product-variation/product-variation-router.ts
@@ -1,6 +1,5 @@
 import Router from '@koa/router';
 import * as controller from './product-variation-controller';
-import { CreateProductVariationSchema, UpdateProductVariationSchema } from 'domains/product-variation/product-variation-schema';
 import { validate, locker } from 'app/middlewares';
 import { makeUuidSchema } from 'domains/common/common-schema';
 
@@ -9,10 +8,6 @@ const router = new Router({
 })
   .use(locker.api());
 
-router.get('/', controller.index);
-router.get('/:uuid', validate.params(makeUuidSchema('uuid')), controller.show);
-router.post('/', validate.body(CreateProductVariationSchema), controller.create);
-router.patch('/:uuid', validate.params(makeUuidSchema('uuid')), validate.body(UpdateProductVariationSchema), controller.update);
-router.delete('/:uuid', controller.destroy);
+router.delete('/:uuid', validate.params(makeUuidSchema('uuid')), controller.destroy);
 
 export default router;
